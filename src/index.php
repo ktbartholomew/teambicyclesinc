dbac2e67e9525607d79bb69d7e2f8c128ec2e3f1
Keith Bartholomew <keith.bartholomew@rackspace.com>
2017-10-09 09:56:46 -0500

    add a cache service and send cache-control headers

--- src/index.php
+++ src/index.php
@@ -5,6 +5,8 @@ namespace Theme;
 use Theme\Helpers\Request;
 use Timber\Timber;
 
+header('cache-control: public, max-age=1200');
+
 
 // This file will be loaded for a number of edge case scenarios for which other
 // template files don't exist. Below we try to identify the edge case and render
